e6ca23584518d945541fb8e0022444d839237967
jose villabona <jjvillabona1@gmail.com>
2019-05-19 13:10:40 -0500

    inicialisando todo

---
+++ codBase/js/app.js
@@ -0,0 +1,14 @@
+var calculadora = {
+  display: document.getElementById("display"),
+	numeroDisplay: "0",
+	operacion: "",
+	operadorA: 0,
+	operadorB: 0,
+	todoValor: 0,
+	resultado: 0,
+	teclaIgual: false,
+
+
+
+
+}
